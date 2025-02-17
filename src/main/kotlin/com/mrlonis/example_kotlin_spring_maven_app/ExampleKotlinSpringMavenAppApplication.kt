014064cb14d00577d218b6eb5eb0df7e40540e87
Matthew Lonis <mattlonis@gmail.com>
2025-02-16 22:29:51 -0500

    Adding kotlin formatting

--- src/main/kotlin/com/mrlonis/example_kotlin_spring_maven_app/ExampleKotlinSpringMavenAppApplication.kt
+++ src/main/kotlin/com/mrlonis/example_kotlin_spring_maven_app/ExampleKotlinSpringMavenAppApplication.kt
@@ -3,9 +3,8 @@ package com.mrlonis.example_kotlin_spring_maven_app
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class ExampleKotlinSpringMavenAppApplication
+@SpringBootApplication class ExampleKotlinSpringMavenAppApplication
 
 fun main(args: Array<String>) {
-	runApplication<ExampleKotlinSpringMavenAppApplication>(*args)
+    runApplication<ExampleKotlinSpringMavenAppApplication>(*args)
 }
